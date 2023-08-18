7c025055751010fd793a4ecb0255210dbdc99e5a
DWilken02 <Dylan.Wilken06@gmail.com>
2023-08-18 11:02:00 -0700

    make changes to JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-window.onload = function(){
+window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
+    event.preventDefault();
     const person1Input = document.getElementById("person1Input").value;
     const person2Input = document.getElementById("person2Input").value;
     const animalInput= document.getElementById("animalInput").value;
     const exclamationInput = document.getElementById("exclamationInput").value;
     const verbInput = document.getElementById("verbInput").value;
+    console.log("verbInput = " + verbInput);
     const nounInput = document.getElementById("nounInput").value;
 
     document.querySelector("span#person1a").innerText = person1Input;
@@ -14,11 +16,13 @@ window.onload = function(){
     document.querySelector("span#person2a").innerText = person2Input;
     document.querySelector("span#person2b").innerText = person2Input;
     document.querySelector("span#animal").innerText = animalInput;
+    console.log("Correctly targeting <span>? = ", document.querySelector("span#verb"));
+    console.log("Correctly targeting innerText? = ", document.querySelector("span#verb").inerText);
     document.querySelector("span#verb").innerText = verbInput;
+    console.log("Correctly referencing verbInput and assigning value of innerText? = ", document.querySelector("span#verb").inerText);
     document.querySelector("span#noun").innerText = nounInput;
     document.querySelector("span#exclamation").innerText = exclamationInput;
-    document.querySelector("div#story").removeAttribute("class");
 
-    event.preventDefault();
+    document.querySelector("div#story").removeAttribute("class");
   };
 };
